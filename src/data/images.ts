a91b925053ad601a9be416e94ffbcf7e76473431
profsam97 <proftoby4real@hotmail.com>
2025-03-16 18:50:10 +0100

    added images for race car

---
+++ src/data/images.ts
@@ -0,0 +1,11 @@
+export const images : string[] = [ 
+    'https://img.freepik.com/free-vector/electric-car-white-background_1308-21368.jpg?ga=GA1.1.1108621750.1741971476&semt=ais_hybrid',
+    'https://img.freepik.com/free-vector/racing-car-orange-color-isolated-background_1308-69652.jpg?ga=GA1.1.1108621750.1741971476&semt=ais_hybrid',
+    'https://img.freepik.com/free-vector/hand-drawn-muscle-car-illustration_23-2149432254.jpg?ga=GA1.1.1108621750.1741971476&semt=ais_hybrid',
+    'https://img.freepik.com/free-psd/yellow-isolated-car_23-2151852892.jpg?ga=GA1.1.1108621750.1741971476&semt=ais_hybrid',
+    'https://img.freepik.com/free-vector/three-dimensional-image-green-car-isolated-white-background_53876-40876.jpg?ga=GA1.1.1108621750.1741971476&semt=ais_hybrid',
+    'https://img.freepik.com/free-vector/hand-drawn-sedan-car-illustration_53876-34797.jpg?ga=GA1.1.1108621750.1741971476&semt=ais_hybrid',
+    'https://img.freepik.com/free-vector/racing-car-yellow-white_1308-41219.jpg?ga=GA1.1.1108621750.1741971476&semt=ais_hybrid',
+    'https://img.freepik.com/free-psd/flat-design-illustration-car_23-2150983058.jpg?ga=GA1.1.1108621750.1741971476&semt=ais_hybrid',
+
+];
